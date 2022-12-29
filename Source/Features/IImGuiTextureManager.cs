db13a26dd64a2e26813889559b94f44fe25580d8
Chris Butler <chrisbutler@polygonhearts.com>
2022-12-29 19:39:11 +0000

    Updated VaultCore Package and fixed features

--- Source/Features/IImGuiTextureManager.cs
+++ Source/Features/IImGuiTextureManager.cs
@@ -6,15 +6,12 @@ namespace VaultCore.ImGuiWindowsAPI;
 /// <summary>
 /// Feature for converting textures for use with ImGui
 /// </summary>
-public interface IImGuiTextureManager : IVaultCoreFeature<IImGuiTextureManager.FeatureApi>
+public interface IImGuiTextureManager : IVaultCoreFeature
 {
-    public interface FeatureApi : IVaultCoreFeatureApi
-    {
-        /// <summary>
-        /// Gets or Creates a ImGuiTextureRef from a Texture2D. These can be used in ImGui calls that need a user texture pointer
-        /// </summary>
-        /// <param name="texture">Texture to create ImGuiTextureRef for</param>
-        /// <returns>ImGuiTextureRef that can be used to use the passed in texture in ImGui calls</returns>
-        public ImGuiTextureRef GetOrCreateImGuiTextureRefForTexture(Texture2D texture);
-    }
+    /// <summary>
+    /// Gets or Creates a ImGuiTextureRef from a Texture2D. These can be used in ImGui calls that need a user texture pointer
+    /// </summary>
+    /// <param name="texture">Texture to create ImGuiTextureRef for</param>
+    /// <returns>ImGuiTextureRef that can be used to use the passed in texture in ImGui calls</returns>
+    public ImGuiTextureRef GetOrCreateImGuiTextureRefForTexture(Texture2D texture);
 }
--- Source/Features/IImGuiWindowManager.cs
+++ Source/Features/IImGuiWindowManager.cs
@@ -5,49 +5,46 @@ namespace VaultCore.ImGuiWindowsAPI;
 /// <summary>
 /// Feature for interfacing with the ImGui Window Manager in VaultGUI
 /// </summary>
-public interface IImGuiWindowManager : IVaultCoreFeature<IImGuiWindowManager.FeatureApi>
+public interface IImGuiWindowManager : IVaultCoreFeature
 {
-    public interface FeatureApi : IVaultCoreFeatureApi
-    {
-        /// <summary>
-        /// Returns true if any window is set as the full screen window
-        /// </summary>
-        public bool IsAnyWindowFullScreen { get; }
-    
-        /// <summary>
-        /// Returns the window that is currently full screen, or null if no window is full screen
-        /// </summary>
-        /// <returns>The window currently fullscreen, or null if none is</returns>
-        public IImGuiWindow? GetFullscreenWindow();
-    
-        /// <summary>
-        /// Registers a window in the manager
-        /// </summary>
-        /// <param name="window">Window to register to drawing</param>
-        public void RegisterWindow(IImGuiWindow window);
-    
-        /// <summary>
-        /// Unregisters a window in the manager
-        /// </summary>
-        /// <param name="window">Window to unregister for drawing</param>
-        public void UnregisterWindow(IImGuiWindow window);
-    
-        /// <summary>
-        /// Sets this window as the full screen window
-        /// </summary>
-        /// <param name="window">Window to set fullscreen</param>
-        public void SetWindowAsFullscreen(IImGuiWindow window);
-    
-        /// <summary>
-        /// Gets a window by its type. If window does not exist, null is returned
-        /// </summary>
-        /// <typeparam name="T">Type of the window to get (Type should inherit from IImGuiWindow)</typeparam>
-        /// <returns>The window of the type requested, or null if no window of that type is registered</returns>
-        public T? GetWindow<T>() where T : IImGuiWindow;
-    
-        /// <summary>
-        /// Clears any window that is full screen
-        /// </summary>
-        public void ClearFullscreenWindow();
-    }
+    /// <summary>
+    /// Returns true if any window is set as the full screen window
+    /// </summary>
+    public bool IsAnyWindowFullScreen { get; }
+
+    /// <summary>
+    /// Returns the window that is currently full screen, or null if no window is full screen
+    /// </summary>
+    /// <returns>The window currently fullscreen, or null if none is</returns>
+    public IImGuiWindow? GetFullscreenWindow();
+
+    /// <summary>
+    /// Registers a window in the manager
+    /// </summary>
+    /// <param name="window">Window to register to drawing</param>
+    public void RegisterWindow(IImGuiWindow window);
+
+    /// <summary>
+    /// Unregisters a window in the manager
+    /// </summary>
+    /// <param name="window">Window to unregister for drawing</param>
+    public void UnregisterWindow(IImGuiWindow window);
+
+    /// <summary>
+    /// Sets this window as the full screen window
+    /// </summary>
+    /// <param name="window">Window to set fullscreen</param>
+    public void SetWindowAsFullscreen(IImGuiWindow window);
+
+    /// <summary>
+    /// Gets a window by its type. If window does not exist, null is returned
+    /// </summary>
+    /// <typeparam name="T">Type of the window to get (Type should inherit from IImGuiWindow)</typeparam>
+    /// <returns>The window of the type requested, or null if no window of that type is registered</returns>
+    public T? GetWindow<T>() where T : IImGuiWindow;
+
+    /// <summary>
+    /// Clears any window that is full screen
+    /// </summary>
+    public void ClearFullscreenWindow();
 }
