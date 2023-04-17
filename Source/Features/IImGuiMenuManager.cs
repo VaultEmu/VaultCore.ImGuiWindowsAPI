298068af534ab4a490875031a2b53be434fe37f4
Chris Butler <chrisbutler@polygonhearts.com>
2023-04-17 22:29:44 +0100

    Added function to ImGuiMenuManager interface to control order of top level menus

--- Source/Features/IImGuiMenuManager.cs
+++ Source/Features/IImGuiMenuManager.cs
@@ -18,4 +18,12 @@ public interface IImGuiMenuManager : IVaultCoreFeature
     /// </summary>
     /// <param name="menuItem">MenuItem to unregister</param>
     public void UnregisterMenuItem(ImGuiMenuItem menuItem);
+    
+    /// <summary>
+    /// Sets the priority of a top level menu item. High the priority the further to the right it will be
+    /// Items not explicitly set will be at Priority 0
+    /// </summary>
+    /// <param name="itemName">Name of the top level menu item (case insensitive)</param>
+    /// <param name="Priority">priority to add the item</param>
+    public void SetTopLevelMenuPriority(string itemName, int Priority);
 }
